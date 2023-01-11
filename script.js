55ba5142df7b585341d394793ff794495b2b1645
Mr.Nayra <aryanedu5786@gmail.com>
2023-01-11 09:45:39 +0530

    changes in button and form functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector("#myForm").addEventListener("submit", function (e) {
+    // Prevent default form submission behavior
+    e.preventDefault();
+  
+    // Show "Thank you" message
+    document.querySelector("#thankYou").style.display = "block";
+  
+    // Remove form
+    this.remove();
+  });
+  
